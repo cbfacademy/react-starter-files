40c487c026362dcd04113b2ee3b71be3e10c20f7
Efua Akumanyi <efua.akumanyi@gmail.com>
2022-05-22 21:59:26 +0100

    Update session 5 to  React version 18. Fix bug in Book component

--- session05/bookcaseapp/src/components/Book.js
+++ session05/bookcaseapp/src/components/Book.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Book() {
-  return "This is the output of a book component";
+  return "This is the output of a book component. Replace this string with code to display an individual book's attributes.";
 }
 
-export default App;
+export default Book;
