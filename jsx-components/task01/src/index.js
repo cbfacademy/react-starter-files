f22914175440ff457f4a37cdcf8daee8bb06dcb3
Efua Akumanyi <efua.akumanyi@gmail.com>
2022-10-18 12:31:41 +0100

    Renamed folder  from session03 to jsx-components

--- jsx-components/task01/src/index.js
+++ jsx-components/task01/src/index.js
@@ -4,18 +4,10 @@ import ReactDOM from 'react-dom/client';
 const name = '[YOUR NAME GOES HERE]';
 
 function Profile(name){
-  //  if(){
-    return <main id="content" role="main" class="base">
+    return <main id="content" role="main" className="base">
             <h1>{name}'s React Page</h1>
               <p>Example of react JSX in action</p>
             </main>
-  //  }
-  //  else {
-  //    return <main id="content" role="main" class="base">
-  //      <h1>Just a React Page</h1>
-  //      <p>Nothing to see here!</p>
-  //    </main>  
-  //  }
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
